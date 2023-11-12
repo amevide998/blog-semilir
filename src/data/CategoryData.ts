5e87b8c51358c102e98bd36ac54f18ab658772a7
amevide998 <hdsvidi@gmail.com>
2023-11-12 14:39:25 +0700

    add get one category

--- src/data/CategoryData.ts
+++ src/data/CategoryData.ts
@@ -1,11 +1,21 @@
 import Category from "@/models/schemas/categorySchema";
 import {connectToDatabase} from "@/databases/mongodb";
+import mongoose from "mongoose";
 
 
 export const getCategories = async () => {
     try{
         await connectToDatabase();
         return await Category.find()
+    }catch (err){
+        console.log("get categories error : ", err)
+    }
+}
+
+export const getCategory = async (id: mongoose.Types.ObjectId) => {
+    try{
+        await connectToDatabase();
+        return await Category.findById(id)
     }catch (err){
         console.log("get categories error : ", err)
     }
