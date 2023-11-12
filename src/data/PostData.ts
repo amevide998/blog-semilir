942da51676ecfeebc65f87f39ca6186d2541c4b3
amevide998 <hdsvidi@gmail.com>
2023-11-12 15:19:23 +0700

    add limit for pagination

--- src/data/PostData.ts
+++ src/data/PostData.ts
@@ -2,10 +2,15 @@ import Post from "@/models/schemas/postSchema";
 import {connectToDatabase} from "@/databases/mongodb";
 
 
-export const getPosts = async () => {
+export const getPosts = async (page: number) => {
     try{
         await connectToDatabase();
-        return await Post.find()
+        const posts =  await Post.find().limit(4).skip(page -1).sort({createdAt: -1})
+        const count = await Post.countDocuments()
+        return {
+            posts,
+            count
+        }
     }catch (err){
         console.log("get categories error : ", err)
     }
