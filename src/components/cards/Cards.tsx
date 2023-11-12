32c570e08517423a666e7dbf82cfb9215ad6b686
amevide998 <hdsvidi@gmail.com>
2023-11-12 14:39:36 +0700

    edit cards

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,7 +1,31 @@
 import styles from "./cards.module.css";
-export default function Cards({page}: {page: number}) {
+import {Card} from "@/components/card/Card";
+import {getPosts} from "@/data/PostData";
+export default async function Cards({page}: {page: number}) {
+
+    const posts = await getPosts()
+
+    if(!posts){
+        return (
+            <>
+                Post Not Found
+            </>
+        )
+    }
 
     return (
-        <div className={styles.container}>Cards, page {page}</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                Recent Post
+            </h1>
+            <div className={styles.posts}>
+                {
+                    posts?.map((item) => (
+                        <Card key={item._id} item={item}/>
+                    ))
+                }
+            </div>
+            {/*<Pagination page={page} hasPrev={hasPrev} hasNext={hasNext}/>*/}
+        </div>
     )
 }
