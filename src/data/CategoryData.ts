d9a9c7317639dfd107483f65e4d489a879cebdd9
amevide998 <hdsvidi@gmail.com>
2023-11-13 14:42:26 +0700

    add getPostByCategories

--- src/data/CategoryData.ts
+++ src/data/CategoryData.ts
@@ -1,6 +1,7 @@
 import Category from "@/models/schemas/categorySchema";
 import {connectToDatabase} from "@/databases/mongodb";
 import mongoose from "mongoose";
+import Post from "@/models/schemas/postSchema";
 
 
 export const getCategories = async () => {
@@ -16,6 +17,34 @@ export const getCategory = async (id: mongoose.Types.ObjectId) => {
     try{
         await connectToDatabase();
         return await Category.findById(id)
+    }catch (err){
+        console.log("get categories error : ", err)
+    }
+}
+
+
+export const getPostByCategories = async (page: number, categories: string) => {
+    try{
+        await connectToDatabase();
+        const cat = await Category.findOne({slug: categories})
+
+        if(!cat){
+            const posts =  await Post.find().limit(4).skip(page -1).sort({createdAt: -1})
+            const count = await Post.countDocuments()
+
+            return {
+                posts,
+                count
+            }
+        }
+
+        const posts =  await Post.find({category:cat._id}).limit(4).skip(page -1).sort({createdAt: -1})
+        const count = await Post.countDocuments({category:cat._id})
+        return {
+            posts,
+            count,
+            cat
+        }
     }catch (err){
         console.log("get categories error : ", err)
     }
