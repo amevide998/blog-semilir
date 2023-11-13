81a939f653f6e1b7b29857759e991ea33d74696b
amevide998 <hdsvidi@gmail.com>
2023-11-13 14:41:47 +0700

    fixing pagination button

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -1,24 +1,34 @@
 "use client"
 import React from "react";
 import styles from "./pagination.module.css";
-import {useRouter} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 
 
-export default function Pagination({page, hasPrev, hasNext, count}: {page: number, hasPrev: boolean, hasNext: boolean, count: number}) {
+export default function Pagination(
+    {page, hasPrev, hasNext, count, cat}:
+        {page: number, hasPrev: boolean, hasNext: boolean, count: number, cat?: string}) {
 
     const router = useRouter();
 
     const totalPage = Math.ceil(count / 4);
 
+    const pathName = usePathname();
+
 
     return (
         <div className={styles.container}>
-            <button className={styles.button} onClick={()=> router.push(`/?page=${page-1}`, {scroll: true})} disabled={!hasPrev} >Previous</button>
+            <button className={styles.button}
+                    onClick={()=>{
+                        router.push(`${pathName=='/blog' ? '/blog': ''}?page=${page-1} ${cat? `&category=${cat}`: ''}`
+                            , {scroll: true})
+                    }}
+                    disabled={!hasPrev}
+            >Previous
+            </button>
             <div className={styles.pageNumber}>
 
             </div>
             <button className={styles.button} onClick={()=> router.push(`/?page=${page+1}`, {scroll: true})} disabled={!hasNext} >Next</button>
-
         </div>
     );
 }
