f40af8c02229809e63f6adca1c15ee649e148030
amevide998 <hdsvidi@gmail.com>
2023-11-23 23:01:51 +0700

    change hero image

--- src/components/featured/FeaturedImage.tsx
+++ src/components/featured/FeaturedImage.tsx
@@ -1,11 +1,13 @@
 import styles from "@/components/featured/featured.module.css";
 import React from "react";
+import Image from "next/image";
 
 export default function FeaturedImage(){
     return (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src="https://firebasestorage.googleapis.com/v0/b/blog-35591.appspot.com/o/image%2Fhero-image-full.png?alt=media&token=30ac8ac9-4b3f-40c3-8dd6-0d3ed0150fbc"
+        <Image src="/hero/hero-image.jpeg"
                alt="post image"
-               className={styles.postImage}/>
+               className={styles.postImage}
+               fill
+        />
     )
 }
