8c543d8d130fda9e82ac4c0216b82aca74237440
amevide998 <hdsvidi@gmail.com>
2023-11-24 00:21:42 +0700

    fixing recent post layout

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -11,15 +11,7 @@ export const Card = async ({item}: {item: any}) => {
         {
         category && (
         <div className={styles.container} key={item._id}>
-            {
-                item.image && (
-                    <div className={styles.imgContainer}>
-                        {/*eslint-disable-next-line @next/next/no-img-element*/}
-                        <img src={item.image} alt={''} className={styles.postImage}/>
-                    </div>
-                )
-            }
-            <div className={styles.textContainer}>
+            <div className={styles.info}>
                 <div className={styles.detail}>
                     <span className={styles.date}>
                         {item.createdAt.toLocaleString('default', {dateStyle: 'long'})}
@@ -27,17 +19,30 @@ export const Card = async ({item}: {item: any}) => {
                     &nbsp;-&nbsp;
                     <span className={styles.category} style={{color: category.color2}}>
                         {category.title}
+                    {/*    todo - tambahin author nya*/}
                     </span>
                 </div>
-                <Link href={`/posts/${item.slug}`}>
-                    <h1 className={styles.cardTitle}>{item.title}
-                    </h1>
-                </Link>
-
-                <div className={styles.cardDescription} dangerouslySetInnerHTML={{__html: `${item.subtitle}`}} />
-                <Link href={`/posts/${item.slug}`} className={styles.readMoreLink}>
-                    Read More
-                </Link>
+            </div>
+            <div className={styles.blogContent}>
+                <div className={styles.imgContainer}>
+                    {/*eslint-disable-next-line @next/next/no-img-element*/}
+                    {item.image && (<img src={item.image} alt={''} className={styles.postImage}/>)}
+                </div>
+                <div className={styles.textContainer}>
+                    <Link href={`/posts/${item.slug}`} className={styles.titleContainer}>
+                        <h1 className={styles.cardTitle}>
+                            {item.title}
+                        </h1>
+                    </Link>
+                    <div className={styles.descriptionContainer}>
+                        <div className={styles.cardDescription} dangerouslySetInnerHTML={{__html: `${item.subtitle}`}} />
+                        <div className={styles.readMoreLink}>
+                            <Link href={`/posts/${item.slug}`}>
+                                Read More
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         )}
