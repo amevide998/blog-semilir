e701ef624d05a7beabd25d5da00f268fe73ebe9f
amevide998 <hdsvidi@gmail.com>
2023-11-25 13:37:21 +0700

    fixing edit page

--- src/components/edit-post/EditPost.tsx
+++ src/components/edit-post/EditPost.tsx
@@ -27,50 +27,63 @@ export default function EditPost({posts, refetch}: any) {
         }
     }
 
-
     return (
         <div className={styles.container}>
             <div className={styles.posts}>
-                {
-                    posts && posts.map((item: any) => (
-                        <div key= {item._id}>
-                        <div  className={styles.postContainer}>
-                             {/*eslint-disable-next-line @next/next/no-img-element*/}
-                            <img src={item.image} alt={""} />
-                            <div className={styles.post}>
-                                <p className={styles.title}>
-                                    {item.title}
-                                </p>
-                                <p className={styles.subtitle}>
-                                    {item.subtitle}
-                                </p>
-                                <div className={styles.info}>
-                                    <div className={styles.views}>
-                                        <p>
-                                            {item.views} views
-                                        </p>
-                                        {/*<Image src={"/view-svgrepo-com.png"}  alt={"eye"} width={12} height={12}/>*/}
-                                    </div>
-                                    <p className={styles.date}>
-                                        {item.createdAt.substring(0,10)}
+                {posts && posts.map((item: any) => (
+                    <div key= {item._id} className={styles.postContainer}>
+                        {/*eslint-disable-next-line @next/next/no-img-element*/}
+                        {
+                            item.image && (<img src={item.image} alt={""} />)
+                        }
+                        <div className={styles.post}>
+                            <div className={styles.tags}>
+                                <div className={styles.views}>
+                                    <p>
+                                        {item.views} views
                                     </p>
                                 </div>
+                                <p className={styles.category} style={{color: item.category.color1}}>
+                                    {item.category.title}
+                                </p>
                             </div>
-                            <div className={styles.edit}>
-                                <Link href={`/write/${item.slug}`}
-                                      className={styles.button}>
-                                    Edit
-                                </Link>
+
+                            <p className={styles.title}>
+                                {item.title}
+                            </p>
+                            <p className={styles.subtitle}>
+                                {item.subtitle}
+                            </p>
+                            <div className={styles.info}>
+                                <p className={styles.date}>
+                                    {item.createdAt.substring(0,10)}
+                                </p>
                             </div>
-                            <div className={styles.delete}>
-                                <button onClick={()=> deleteHandler(item._id)}
-                                      className={styles.button}>
-                                    Delete
-                                </button>
+                            <div className={styles.postControls}>
+                                <div className={styles.postControlButton}>
+                                    <div className={styles.edit}>
+                                        <Link href={`/write/${item.slug}`}
+                                              className={styles.button}>
+                                            Edit
+                                        </Link>
+                                    </div>
+                                    <div className={styles.delete}>
+                                        <button onClick={()=> deleteHandler(item._id)}
+                                                className={styles.deleteButton}>
+                                            Delete
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className={styles.info}>
+                                    {item.published ?
+                                        (<p className={styles.published}>Published</p>)
+                                        :
+                                        (<p className={styles.draft}>Draft</p>)
+                                    }
+                                </div>
                             </div>
                         </div>
-                    <hr />
-                        </div>
+                    </div>
                     ))
                 }
             </div>
